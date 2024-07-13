f0652c52040331f7ca3fea81ca8ac85ad979c367
Bernard Dubuisson <bernard@dub.be>
2024-07-13 20:45:08 +0200

    aws fix

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,9 +1,9 @@
-require 'aws-sdk-s3'
+# require 'aws-sdk-s3'
 
-Aws.config[:credentials] = Aws::Credentials.new(
-  ENV['BUCKETEER_AWS_ACCESS_KEY_ID'],
-  ENV['BUCKETEER_AWS_SECRET_ACCESS_KEY']
-)
-Aws.config[:region]      = 'eu-west-3'
-
-S3 = Aws::S3::Client.new
+# Aws.config[:credentials] = Aws::Credentials.new(
+#   ENV['BUCKETEER_AWS_ACCESS_KEY_ID'],
+#   ENV['BUCKETEER_AWS_SECRET_ACCESS_KEY']
+# )
+# Aws.config[:region]      = 'eu-west-3'
+#
+# S3 = Aws::S3::Client.new
